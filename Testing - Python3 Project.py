ccfc065465a40c49a629b83bb4b412dc993c30ce
Vasile <31428010+Vasile11416@users.noreply.github.com>
2021-11-20 17:15:46 -0500

    Add files via upload

---
+++ Testing - Python3 Project.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+print('hello world') # hey there, welcome to my Python Project :)
+
+
+# In[ ]:
+
+
+
+
+
+# In[21]:
+
+
+import pandas as pd  #rename pandas w/ alias
+df = pd.read_csv('vgsales.csv')
+df.shape
+
+
+# In[22]:
+
+
+import pandas as pd  
+df = pd.read_csv('vgsales.csv')
+df
+
+
+# In[23]:
+
+
+df.describe ()
+
+
+# In[24]:
+
+
+df.values
+
+
+# In[28]:
+
+
+df.describe()a
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
